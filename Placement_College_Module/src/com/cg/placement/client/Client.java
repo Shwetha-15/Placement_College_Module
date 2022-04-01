c20dcc490e90b0c5738ed55a257cf192b1684440
Shwetha-15 <shwetha151998@gmail.com>
2022-04-01 14:57:08 +0530

    1st commit

--- Placement_College_Module/src/com/cg/placement/client/Client.java
+++ Placement_College_Module/src/com/cg/placement/client/Client.java
@@ -2,6 +2,7 @@ package com.cg.placement.client;
 
 import com.cg.placement.entities.College;
 import com.cg.placement.service.CollegeServiceImpl;
+import com.cg.placement.service.ICollegeService;
 
 public class Client 
 {
@@ -9,6 +10,36 @@ public class Client
     {
 		College college = new College();
 		
-		CollegeServiceImpl service = new CollegeServiceImpl();
+		ICollegeService service = new CollegeServiceImpl();
+		
+		//Create
+		college.setId(10);
+		college.setCollegename("NMIT");
+		//college.setCollegeAdmin(User);
+		college.setLocation("Bangalore");
+		
+		college.setId(11);
+		college.setCollegename("SVCE");
+		//college.setCollegeAdmin(User);
+		college.setLocation("Mumbai");
+		
+		//Retrieve
+		college = service.searchCollege(10);
+		System.out.println("ID is: "+college.getId());
+		System.out.println("College Name is: "+college.getCollegename());
+		System.out.println("College Admin is: "+college.getCollegeAdmin());
+		System.out.println("College Location is: "+college.getLocation());
+		
+		//Update
+		college = service.searchCollege(10);
+		college.setCollegename("Reva University");
+		service.updateCollege(college);
+		System.out.println("Update is successful");
+		
+		//Delete
+		college = service.searchCollege(11);
+		service.deleteCollege(11);
+		System.out.println("Delete is successful");
+		
 	}
 }
